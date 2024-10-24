60882ccff1dc71074ee6d52da985c7bd012066d9
Megan McShane <m-mcshane.1@onu.edu>
2024-10-24 13:15:39 -0400

    Completed countCharacter function

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,19 @@
-// Laboratory 8 
+// Laboratory 8
 // Author: Megan McShane
 // Date: 24th October 2024
-
 // P8-1
 
+//comment
+#include <iostream>
+#include <string>
+using namespace std;
+
+int countCharacter(string str) {
+    return str.length(); // Return the length of the string
+}
+
+int main() {
+    string testStr = "The quick brown fox"; // Test case
+    cout << "Number of characters: " << countCharacter(testStr) << endl;
+    return 0;
+}
