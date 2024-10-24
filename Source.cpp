7100439792d10b4f6445d83ca3ca6f667eb92667
Megan McShane <m-mcshane.1@onu.edu>
2024-10-24 13:19:15 -0400

    Completed countVowel function

--- Source.cpp
+++ Source.cpp
@@ -3,17 +3,23 @@
 // Date: 24th October 2024
 // P8-1
 
-//comment
 #include <iostream>
 #include <string>
 using namespace std;
 
-int countCharacter(string str) {
-    return str.length(); // Return the length of the string
+int countVowel(string str) {
+    int count = 0;
+    for (char c : str) {
+        c = tolower(c); // Convert character to lowercase
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            count++;
+        }
+    }
+    return count; // Return the number of vowels
 }
 
 int main() {
     string testStr = "The quick brown fox"; // Test case
-    cout << "Number of characters: " << countCharacter(testStr) << endl;
+    cout << "Number of vowels: " << countVowel(testStr) << endl;
     return 0;
 }
